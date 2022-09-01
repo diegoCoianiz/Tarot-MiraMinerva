cade2c1231a5ba1aa9a7e75780996cff514b00d8
Juan Diego Coianiz <diego.c.coianiz@gmail.com>
2022-09-01 11:58:28 -0300

    completada la lista de valores de cartas, preparando el html

--- public/js/index.js
+++ public/js/index.js
@@ -5,29 +5,27 @@ const IdMazo = document.getElementById("IdMazo")
 //#region posiciones y significados
 const Carta = {
     _1 : [ " Sé tú mismo", " inventando tu propio camino que te lleve a", " ser Tú Mismo."],
-_2 : [ " Transforma", " transformandote en función de", " tus objetivos."],
-_4 : [ " Habita tu espacio vital" , " generando", " tu espacio vital."],
-_5 : [ " Domina tus capacidades" , " avanzando hacia", " lo que mereces."],
-_3 : [ " Custodia lo que más amas" , " custodiando" , " lo sagrado que hay en tí."],
-_6 : [ " Construye", " construyendo el camino que te lleve a", " lo que te lleve más allá de lo conocido."],
-_7 : [ " Usa tu libre albedrío", " eligiendo libremente", " el uso de tu libre albedrío."], 
-_8 : [ " Avanza", " definiendo las estrategias para", " el éxito de tus batallas."], 
-_9 : [ " Define los límites", " comprometiéndote a aceptar", " el respeto de los límites."],
-_10 : [ " Aprende de tus experiencias", " aprendiendo de tus experiencias lo que necesites saber sobre", " el camino que recorres."],
-_11 : [ "Mantén en tu lugar", " manteniéndote en equilibrio con", " el reconocimiento de las vicisitudes de la vida."],
-_12 : [ " Sostente", " sosteniendo", " tu fortaleza."],
-_13 : [ "Tómate tu tiempo", " poniendo en pausa", " las pausas necesarias."],
-_14 : [ " Cierra el ciclo", " poniéndole fin a", " el final para que algo nuevo nazca."],
-_15 : [ " Cambia", " revirtiendo", " los cambios que sean necesarios."],
-_16 : [ " Seduce", " capturando", " lo que te seduce."],
-_17 : [ " Rompe con los condicionamientos", " rompiendo con los condicionamientos que te alejan de", " tu último refugio."], 
-_18 : [ " Entrégate sin temor", " entregándote sin temor a", " lo más genuino de tí." ],
-_19 : [ " Guarda tus secretos", "ocultando que lo que te impulsa es", "tus misterios." ],
-_20 : [ " Echa luz", " observando", " tus compromisos."],
-_21 : [ " Revisa", "revisando", " tus juicios de valor."],
-_22 : [ " Alcanza tus metas", " alcanzando", " tus actuales propósitos."]
-
-    
+    _2 : [ " Transforma", " transformandote en función de", " tus objetivos."],
+    _4 : [ " Habita tu espacio vital" , " generando", " tu espacio vital."],
+    _5 : [ " Domina tus capacidades" , " avanzando hacia", " lo que mereces."],
+    _3 : [ " Custodia lo que más amas" , " custodiando" , " lo sagrado que hay en tí."],
+    _6 : [ " Construye", " construyendo el camino que te lleve a", " lo que te lleve más allá de lo conocido."],
+    _7 : [ " Usa tu libre albedrío", " eligiendo libremente", " el uso de tu libre albedrío."], 
+    _8 : [ " Avanza", " definiendo las estrategias para", " el éxito de tus batallas."], 
+    _9 : [ " Define los límites", " comprometiéndote a aceptar", " el respeto de los límites."],
+    _10 : [ " Aprende de tus experiencias", " aprendiendo de tus experiencias lo que necesites saber sobre", " el camino que recorres."],
+    _11 : [ "Mantén en tu lugar", " manteniéndote en equilibrio con", " el reconocimiento de las vicisitudes de la vida."],
+    _12 : [ " Sostente", " sosteniendo", " tu fortaleza."],
+    _13 : [ "Tómate tu tiempo", " poniendo en pausa", " las pausas necesarias."],
+    _14 : [ " Cierra el ciclo", " poniéndole fin a", " el final para que algo nuevo nazca."],
+    _15 : [ " Cambia", " revirtiendo", " los cambios que sean necesarios."],
+    _16 : [ " Seduce", " capturando", " lo que te seduce."],
+    _17 : [ " Rompe con los condicionamientos", " rompiendo con los condicionamientos que te alejan de", " tu último refugio."], 
+    _18 : [ " Entrégate sin temor", " entregándote sin temor a", " lo más genuino de tí." ],
+    _19 : [ " Guarda tus secretos", "ocultando que lo que te impulsa es", "tus misterios." ],
+    _20 : [ " Echa luz", " observando", " tus compromisos."],
+    _21 : [ " Revisa", "revisando", " tus juicios de valor."],
+    _22 : [ " Alcanza tus metas", " alcanzando", " tus actuales propósitos."]
 }
 //#endregion
 
@@ -136,13 +134,16 @@ devolucion_2 = (l1, l2, l3) => {
     console.log("en devolucion dos: llegan los 3 elementos: ", l1, l2, l3)
     const devolucion1 = document.getElementById("devolucion1")
     devolucion1.remove()
-    console.log(Carta["_"+l1],Carta["_"+l2],Carta["_"+l3])
+    console.log("las siguientes son las cartas elejidas:")
+    console.log(Carta["_"+(l1+1)],Carta["_"+(l2+1)],Carta["_"+(l3+1)])
+    console.log("y estos sus significados:")
+    console.log(Carta["_"+(l1+1)][0],", ",Carta["_"+(l2+1)][1],Carta["_"+(l3+1)][2])
     
     InitPogram.innerHTML =
     `
         <div id="devolucion2">
-            <h1 style="c    olor:black;">
-                ${Carta["_"+l1][0]}, ${Carta["_"+l2][1]} ${Carta["_"+l3][2]}
+            <h1 style="color:black;">
+                ${Carta["_"+(l1+1)][0]}, ${Carta["_"+(l2+1)][1]} ${Carta["_"+(l3+1)][2]}
             </h1>
         </div>
     `
