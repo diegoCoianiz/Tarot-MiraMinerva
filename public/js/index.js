c52b435592445f19cc3a9185da91ab011be02d4a
Juan Diego Coianiz <diego.c.coianiz@gmail.com>
2022-09-01 11:23:40 -0300

    trabajando sobre diseño de la devolucion2

--- public/js/index.js
+++ public/js/index.js
@@ -3,9 +3,20 @@ const firstStep = document.getElementById("firstStep")
 const IdMazo = document.getElementById("IdMazo")
 
 //#region posiciones y significados
-const carta = {
-    _1 : ["uno", "dos", "tres"],
-    _2 : ["cuatro", "cinco", "seis"]
+const Carta = {
+    _0 : ["","",""],
+    _1 : ["","",""],
+    _2 : [ " Transforma", " transformando", " tus objetivos."],
+    _4 : [ " Habita tu espacio vital" , " desarrollando", " tu espacio vital."],
+    _5 : [ " Domina tus capacidades" , " avanzando hacia", " lo que mereces."],
+    _3 : [ " Custodia lo que más amas" , " custodiando" , " lo sagrado que hay en tí."],
+    _6 : [ " Construye", " construyendo el camino que te lleve a", " lo que te lleve más allá de lo que dominas."],
+    _7 : [ " Usa tu libre albedrío", " eligiendo libremente", " el uso de tu libre albedrío."], 
+    _8 : [ " Avanza", " definiendo las estrategias para", " el éxito de tus batallas."], 
+    _9 : [ " Define los límites", " comprometiéndote con", " el respeto de los límites."],
+    _10 : [ " Aprende de tus experiencias", " aprendiendo de tus experiencias lo que necesites saber sobre", " el camino que recorres."],
+    _11 : [ " Sostiene", " sosteniendo", " tu fortaleza."]
+    
 }
 //#endregion
 
@@ -78,36 +89,52 @@ seleccion = (i) => {
 }
 
 devolucion_1 = (lis) => {
+    console.log("en devolucion uno: llegan los elementos: ", lis)
     IdTodo.remove()
     document.body.style.backgroundImage = "url('img/fondo.jpg')"
     InitPogram.innerHTML = 
         `
-        <div class="row row-cols-4" style="height:250px; color:white;">
+        <div id="devolucion1">
+            <div class="row row-cols-4" style="height:250px; color:white;">
 
-        </div>
+            </div>
 
-        <div class="row row-cols-4" style="height:250px; color:black;" onclick="devolucion_2(${lis})">
-            <div class="col">
+            <div class="row row-cols-4" style="height:250px; color:black;" onclick="devolucion_2(${lis})">
+                <div class="col">
+                </div>
+                <div class="col col-6 mx-1">
+                    <h1> El consejo que te da el Tarot es: </h1>
+                </div>
             </div>
-            <div class="col col-6 mx-1">
-                <h1> El consejo que te da el Tarot es: </h1>
-            </div>
-        </div>
 
-        <div class="row row-cols-6" style="color:black;">
-            <div class="col-5">
+            <div class="row row-cols-6" style="color:black;">
+                <div class="col-5">
+                </div>
+                <div class="col mx-1">
+                    <h2>(click)</h2>
+                </div>
+                <div class="col">
             </div>
-            <div class="col mx-1">
-                <h2>(click)</h2>
-            </div>
-            <div class="col">
         </div>
     </div>
-        `
+    `
 } 
 
-devolucion_2 = () => {
+devolucion_2 = (l1, l2, l3) => {
 
+    console.log("en devolucion dos: llegan los 3 elementos: ", l1, l2, l3)
+    const devolucion1 = document.getElementById("devolucion1")
+    devolucion1.remove()
+    console.log(Carta["_"+l1],Carta["_"+l2],Carta["_"+l3])
+    
+    InitPogram.innerHTML =
+    `
+        <div id="devolucion2">
+            <h1 style="c    olor:black;">
+                ${Carta["_"+l1][0]}, ${Carta["_"+l2][1]} ${Carta["_"+l3][2]}
+            </h1>
+        </div>
+    `
 }
 
 startApp()
